f5dd11797c4f9bf5c4878c4da8569d23691ea6ed
Vania Dimitrova <vani4ka66@abv.bg>
2017-12-05 15:25:53 +0200

    Create 02. Even-Position-Element.js

---
+++ 06. Arrays-and-Matrices/02. Even-Position-Element.js
@@ -0,0 +1,16 @@
+function solve(input) {
+
+    let arr = [];
+
+    for (let i = 0; i < input.length; i++) {
+        if(i%2 == 0){
+            arr.push(input[i]);
+        }
+
+    }
+    console.log(arr.join(' '));
+
+}
+
+solve(['20', '30', '40']
+);
