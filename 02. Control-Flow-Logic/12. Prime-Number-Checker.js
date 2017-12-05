f3572742f9d7a75b25c612aa4ce6664ab56377f1
Vania Dimitrova <vani4ka66@abv.bg>
2017-12-05 14:52:00 +0200

    Create 12. Prime-Number-Checker.js

---
+++ 02. Control-Flow-Logic/12. Prime-Number-Checker.js
@@ -0,0 +1,13 @@
+function isPrime(num) {
+    let prime = true;
+    for (let d = 2; d <=
+    Math.sqrt(num); d++) {
+        if (num % d == 0) {
+            prime = false;
+            break;
+        }
+    }
+    console.log( prime && (num > 1));
+}
+
+isPrime(8);
