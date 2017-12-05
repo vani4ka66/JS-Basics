d16bfac5bfc133ec5ece03b0da7743a65fd6b04e
Vania Dimitrova <vani4ka66@abv.bg>
2017-12-05 14:39:41 +0200

    Create 07. Next-day.js

---
+++ 01. Intro-to-JavaScript/07. Next-day.js
@@ -0,0 +1,18 @@
+function calcNextDay(year, month, day) {
+
+
+
+   let date = new Date(year, month - 1, day);
+
+   let oneDay = 24 * 60 * 60 * 1000;
+
+   let nextDate = new Date(date.getTime() + oneDay);
+
+
+
+   console.log
+(nextDate.getFullYear() + "-" + (nextDate.getMonth() + 1) + "-" + nextDate.getDate())
+
+}
+
+calcNexrDay(2016, 9, 30);
