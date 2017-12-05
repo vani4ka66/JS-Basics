7d8908f459df0c5534e95235f461c2f2598e212a
Vania Dimitrova <vani4ka66@abv.bg>
2017-12-05 15:08:41 +0200

    Create 05. Functional-Calculator.js

---
+++ 04. Functions-and-Arrow-Functions/05. Functional-Calculator.js
@@ -0,0 +1,13 @@
+function solve(n1, n2, operator) {
+
+
+    switch (operator){
+
+        case '+': console.log(n1 + n2);
+        case '-': console.log(n1 - n2);
+        case '*': console.log(n1 * n2);
+        case '/': console.log(n1 / n2);
+    }
+}
+
+solve(3, 3,'/');
