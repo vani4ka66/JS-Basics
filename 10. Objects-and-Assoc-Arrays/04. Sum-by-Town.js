fb2e633dea007161013daeb9ba5e4f7bfaa39991
Vania Dimitrova <vani4ka66@abv.bg>
2017-12-05 16:30:46 +0200

    Create 04. Sum-by-Town.js

---
+++ 10. Objects-and-Assoc-Arrays/04. Sum-by-Town.js
@@ -0,0 +1,15 @@
+function solve(arr) {
+
+let sum = {};
+    for (var i = 0; i < arr.length; i+=2) {
+        let [town, income] = [arr[i], Number(arr[i + 1])];
+
+        if(sum[town] == undefined){
+            sum[town] = income;
+        }
+        else {
+            sum[town] += income;
+        }
+    }
+    console.log(JSON.stringify(sum));
+}
