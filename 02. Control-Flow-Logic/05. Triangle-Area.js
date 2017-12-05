0a34436ac28730b0a267062324f6f0b9a149685c
Vania Dimitrova <vani4ka66@abv.bg>
2017-12-05 14:45:58 +0200

    Create 05. Triangle-Area.js

---
+++ 02. Control-Flow-Logic/05. Triangle-Area.js
@@ -0,0 +1,8 @@
+function triangle(a, b, c) {
+
+        [a, b, c] = [a, b, c].map(Number);
+        let sp = (a + b + c) / 2;
+        let area =
+            Math.sqrt(sp * (sp - a) * (sp - b) * (sp - c));
+        console.log(area);
+    }
