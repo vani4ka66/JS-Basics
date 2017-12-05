25c1ea4f924b6c8a77868e72e8c052ad33c3c39b
Vania Dimitrova <vani4ka66@abv.bg>
2017-12-05 16:09:49 +0200

    Create 13. Match-the-Dates.js

---
+++ 08. Strings-and-Regular-Expressions/13. Match-the-Dates.js
@@ -0,0 +1,17 @@
+function solve(input) {
+
+let pattern = /\b([0-9]{1,2})-([A-Z][a-z]{2})-([0-9]{4})\b/g;
+
+while (match = pattern.exec(input)){
+
+    console.log(`${match[0]} (Day: ${match[1]}, Month: ${match[2]}, Year: ${match[3]})`)
+}
+
+}
+
+solve([
+    'I am born on 30-Dec-1994.',
+    'This is not date: 512-Jan-1996.',
+    'My father is born on the 29-Jul-1955.'
+
+]);
