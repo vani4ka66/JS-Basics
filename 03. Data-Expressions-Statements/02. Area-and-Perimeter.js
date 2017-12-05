ab259792c4cc17a0f1b5117884e563e43d01c9e2
Vania Dimitrova <vani4ka66@abv.bg>
2017-12-05 14:56:36 +0200

    Create 02. Area-and-Perimeter.js

---
+++ 03. Data-Expressions-Statements/02. Area-and-Perimeter.js
@@ -0,0 +1,11 @@
+function rectangle(a, b) {
+
+    a = Number(a);
+    b = Number(b);
+
+    let area = a* b;
+    let perimeter = 2*(a + b);
+
+    console.log(area);
+    console.log(perimeter);
+}
